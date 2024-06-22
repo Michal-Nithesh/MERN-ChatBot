3c67d498fb4531697494fe80baf3565905a19c25
Nithesh14 <michalnithesh@gmail.com>
2024-06-22 16:17:33 +0530

    install Morgan Lib

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import { config } from 'dotenv';
+import mrogan from 'morgan';
 config();
 const app = express();
 // middleware
 app.use(express.json());
+//remove it in production
+app.use(mrogan("dev"));
+app.use("/api/v1");
 export default app;
 //# sourceMappingURL=app.js.map
